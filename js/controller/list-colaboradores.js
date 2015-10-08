32fa4a564d66b0d0004848e3969fc24326684b1d
Filipe Coelho <filipe.mendonca.coelho@gmail.com>
2015-10-08 16:04:58 -0300

    melhoria para diferenciar fotos do sexo masculino e feminino

--- js/controller/list-colaboradores.js
+++ js/controller/list-colaboradores.js
@@ -1,16 +1,19 @@
 configBootstrapTable();
 
-function fotoFormatter(value) {
+function fotoFormatter(value, row, index) {
 	if(value != null && value != "") {
 		if(window.location.hostname == "localhost")
 			value = value.replace("/home/consorciointermultip/public_html/files/docs/", "../sig-backoffice-files/");
 		else
 			value = value.replace("/home/consorciointermultip/public_html/", "");
 	}
-	else
+	else if(row.flg_sexo == "M")
 		value = "img/av1.png";
+	else if(row.flg_sexo == "F")
+		value = "img/av6.png";
+	else
+		value = "img/logo_intermultiplas.jpg";
 
-	console.log(value);
 	return (value != null && value != "") ? '<img src="'+ value +'" class="img-profile-table">' : '';
 }
 
