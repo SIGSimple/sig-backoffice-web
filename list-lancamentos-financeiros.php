c0fe1a55669096859508da77e8081f4eeeff8eed
Filipe Coelho <filipe.mendonca.coelho@gmail.com>
2015-11-26 17:56:29 -0200

    minor features

--- list-lancamentos-financeiros.php
+++ list-lancamentos-financeiros.php
@@ -4,17 +4,19 @@
 	<div class="panel-body">
 		<div class="pad-btm form-inline">
 			<div class="row">
-				<div class="col-xs-12 col-sm-3 col-md-3 col-lg-2">
+				<div class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
 					<div class="input-group date">
+						<span class="input-group-addon">De:</span>
 						<input type="text" class="form-control" ng-model="filtro.dta_inicio" placeholder="De">
-						<span class="input-group-addon"><i class="fa fa-calendar fa-lg"></i></span>
+						<span class="input-group-addon btn-default"><i class="fa fa-calendar fa-lg"></i></span>
 					</div>
 				</div>
 
-				<div class="col-xs-12 col-sm-3 col-md-3 col-lg-2">
+				<div class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
 					<div class="input-group date">
+						<span class="input-group-addon">Até:</span>
 						<input type="text" class="form-control" ng-model="filtro.dta_fim" placeholder="Até">
-						<span class="input-group-addon"><i class="fa fa-calendar fa-lg"></i></span>
+						<span class="input-group-addon btn-default"><i class="fa fa-calendar fa-lg"></i></span>
 					</div>
 				</div>
 
